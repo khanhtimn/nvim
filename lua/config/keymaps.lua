12dd8fad10735cda9ea688231881e076cc0182d8
KhanhTIMN <khanhlcbb@gmail.com>
2023-11-23 18:12:15 +0700

    NeoVim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+OTPS = { noremap = true, silent = true }
+
+-- Ctrl-C / Ctrl-V
+vim.keymap.set({ "n", "v", "x" }, "<C-v>", '"+p', OTPS)
+
+vim.keymap.set({ "n", "v", "x" }, "<C-c>", '"*y', OTPS)
+
+--  all line
+vim.keymap.set({ "n", "v", "x" }, "<C-q>", '"*yy', OTPS)
+
+-- Select all (Ctrl-A)
+vim.keymap.set({ "n", "v", "x" }, "<C-a>", "ggVG")
+
+--Switch tabs
+vim.keymap.set("n", "<C-tab>", ":tabnex<Return>", OTPS)
+
+--New tab
+vim.keymap.set("n", "<C-t>", "tabedit", OTPS)
