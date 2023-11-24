04f16fee8bc65e9a6550f3e34abc065e0a27ec70
KhanhTIMN <khanhlcbb@gmail.com>
2023-11-25 02:48:35 +0700

    Ident

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+--vim.opt.relativenumber = false
+vim.opt.scrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Underline curl
 vim.cmd([[let &t_Cs = "\e[4:3m]"]])
 vim.cmd([[let &t_Ce = "\e[4:0m]"]])
