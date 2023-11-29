faa2d2545a9a13ebd4a0cb20cca9ac7390725ead
phmtrang <trangphamlclc@gmail.com>
2023-11-29 07:32:33 +0700

    Rebase for mac

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -30,7 +30,7 @@ return {
         "rcarriga/nvim-notify",
         opts = {
             render = "minimal",
-            fps = 144,
+            fps = 60,
             timeout = 5000,
             background_colour = "#000000",
         },
