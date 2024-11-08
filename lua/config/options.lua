0678b045fcf424e268fe03fbb5cfe06811909bcb
KhanhTIMN <khanhlcbb@gmail.com>
2024-11-09 01:49:10 +0700

    fzf-native

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@
 vim.o.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
 
 local powershell_options = {
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellcmdflag = "-ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
   shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellquote = "",
---
+++ lua/config/rust.lua
@@ -0,0 +1,30 @@
+vim.g.rustaceanlocal = {
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        cargo = {
+          features = "all",
+        },
+        -- Add the following line to enable rustfmt on save
+        ["rust-analyzer.rustfmt.overrideCommand"] = { "rustfmt", "--edition", "2021", "--emit=stdout" },
+
+        -- Taken from https://book.leptos.dev/getting_started/leptos_dx.html
+        -- Other Settings ...
+        procMacro = {
+          ignored = {
+            leptos_macro = {
+              -- optional: --
+              -- "component",
+              "server",
+            },
+          },
+        },
+      },
+    },
+  },
+}
